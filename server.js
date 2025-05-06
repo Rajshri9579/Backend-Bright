b1c360a0a117ff3c2540b894bdd5bd0d27dd0aef
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-05-06 23:02:54 +0530

    update with expense tracker started

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import {createServer} from 'node:http'
 import fs from 'node:fs'
+import fsPromises from 'node:fs/promises'
 
-const server = createServer( (req, res)=>{
+const server = createServer(async (req, res)=>{
     //ROuting....
     // console.log('req', req);
 
@@ -11,13 +12,47 @@ const server = createServer( (req, res)=>{
 
         const dataStraem = fs.createReadStream('./index.html')
 
-        dataStraem.on('data', (chunk)=>{
-            res.write()
-        })
+        dataStraem.pipe(res)
 
-        res.end(data)
+        // dataStraem.on('data', (chunk)=>{
+        //     res.write(chunk)
+        // })
+
+        // dataStraem.on('end', ()=>{
+        //     res.end()
+        // })
+
+        
     }else if(req.url === '/about'){
+        res.writeHead(200, {'content--type': 'text/html'})
         res.end('<h1>This is your about page<h1>')
+    }else if(req.url === '/expenses'){
+        //Api
+        //create an expense 
+        //POST
+
+        if(req.method === 'POST'){
+            //read data from rrequest
+            let buff = ''
+            req.on('data', (chunk)=>{
+                console.log('chunk ', chunk);
+                buff = buff + chunk.toString()
+            }) 
+
+
+            req.on('end', async()=>{
+                const data = await fsPromises.readFile('./db.json')
+                const dbData = JSON.parse(data)
+                dbData.push(JSON.parse(buff))
+                await fsPromises.writeFile('./db.json', JSON.stringify(dbData, null, 2))
+                res.end('OK')
+            })
+            //store it in json database
+
+        }else if(req.method === 'GET'){
+            //read data from json db 
+            //return the data to client
+        }
     }
     
     console.log('Request received');
