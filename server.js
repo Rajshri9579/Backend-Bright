58a7da70193264e0eba70e8aefe6870cc980a390
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-05-10 16:30:44 +0530

    streaming added

--- server.js
+++ server.js
@@ -1,61 +1,83 @@
 import {createServer} from 'node:http'
 import fs from 'node:fs'
-import fsPromises from 'node:fs/promises'
+// import fsPromises from 'node:fs/promises'
 
+let count = 0;
 const server = createServer(async (req, res)=>{
-    //ROuting....
+    //server sent events (SSE)
+    if(req.url === "/"){
+        
+        const htmlPage = fs.createReadStream('./stream.html')
+
+        htmlPage.pipe(res)
+    }else if(req.url === '/stream'){
+        res.writeHead(200, {
+            'content-type': 'text/event-stream',
+            'cache-control': 'no-cache',
+            connection: 'keep-alive',
+        
+        })
+
+        setInterval(()=>{
+            res.write(`data: The count is - ${count++} \n\n`)
+        }, 1000)
+    }
+        
+
+
+    //Routing....
     // console.log('req', req);
 
-    if(req.url === '/'){
-        res.writeHead(200, {'content--type': 'text/html'})
-        // const data = await fs.readFile('./index.html')
+    // if(req.url === '/'){
+    //     res.writeHead(200, {'content--type': 'text/html'})
+    //     // const data = await fs.readFile('./index.html')
 
-        const dataStraem = fs.createReadStream('./index.html')
+    //     const dataStraem = fs.createReadStream('./index.html')
 
-        dataStraem.pipe(res)
+    //     dataStraem.pipe(res)
 
-        // dataStraem.on('data', (chunk)=>{
-        //     res.write(chunk)
-        // })
+    //     // dataStraem.on('data', (chunk)=>{
+    //     //     res.write(chunk)
+    //     // })
 
-        // dataStraem.on('end', ()=>{
-        //     res.end()
-        // })
+    //     // dataStraem.on('end', ()=>{
+    //     //     res.end()
+    //     // })
 
         
-    }else if(req.url === '/about'){
-        res.writeHead(200, {'content--type': 'text/html'})
-        res.end('<h1>This is your about page<h1>')
-    }else if(req.url === '/expenses'){
-        //Api
-        //create an expense 
-        //POST
+    // }else if(req.url === '/about'){
+    //     res.writeHead(200, {'content--type': 'text/html'})
+    //     res.end('<h1>This is your about page<h1>')
+    // }else if(req.url === '/expenses'){
+    //     //Api
+    //     //create an expense 
+    //     //POST
 
-        if(req.method === 'POST'){
-            //read data from rrequest
-            let buff = ''
-            req.on('data', (chunk)=>{
-                console.log('chunk ', chunk);
-                buff = buff + chunk.toString()
-            }) 
+    //     if(req.method === 'POST'){
+    //         //read data from rrequest
+    //         let buff = ''
+    //         req.on('data', (chunk)=>{
+    //             console.log('chunk ', chunk);
+    //             buff = buff + chunk.toString()
+    //         }) 
 
 
-            req.on('end', async()=>{
-                const data = await fsPromises.readFile('./db.json')
-                const dbData = JSON.parse(data)
-                dbData.push(JSON.parse(buff))
-                await fsPromises.writeFile('./db.json', JSON.stringify(dbData, null, 2))
-                res.end('OK')
-            })
-            //store it in json database
+    //         req.on('end', async()=>{
+    //             const data = await fsPromises.readFile('./db.json')
+    //             const dbData = JSON.parse(data)
+    //             dbData.push(JSON.parse(buff))
+    //             await fsPromises.writeFile('./db.json', JSON.stringify(dbData, null, 2))
+    //             res.end('OK')
+    //         })
+    //         //store it in json database
 
-        }else if(req.method === 'GET'){
-            //read data from json db 
-            //return the data to client
-        }
-    }
+    //     }else if(req.method === 'GET'){
+    //         //read data from json db 
+    //         //return the data to client
+    //     }
+    // }
     
-    console.log('Request received');
+    // console.log('Request received');
     
 })
 
